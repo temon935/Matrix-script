4fc8d6026dffa085ffca26c16ac57da9a2562d05
temon935 <temon935@gmail.com>
2020-07-14 19:51:05 +0300

    Solved

--- Matrix script.py
+++ Matrix script.py
@@ -16,7 +16,16 @@ n = int(first_multiple_input[0])
 m = int(first_multiple_input[1])
 
 matrix = []
-
+text = ''
 for _ in range(n):
     matrix_item = input()
     matrix.append(matrix_item)
+counter = 0
+while counter != m:
+    for i in matrix:
+        text += i[counter]
+    counter += 1
+print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", text))
+
+
+
